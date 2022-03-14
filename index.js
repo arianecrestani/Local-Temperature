ade5f36a2c7d49bf28dfbc5b574c155a585b7aed
Ariane Crestani <ariane.mrv@gmail.com>
2022-03-14 16:02:07 +0100

    created request info and updateUi

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
+const inputCity = document.getElementById("inputCity");
+const generate = document.getElementById("generate");
 
-const getGeonames = async (city) => {
-    const baseUrl = "http://api.geonames.org/postalCodeSearchJSON?";
-    const apiKey = "ariane";
-  
-    return await fetch(`${baseUrl}username=${apiKey}&placename=${city}`)
-        .then((response) => response.json())  // const products = await response.json
-        .catch((error) => console.log(error));
-       
-  };
+const getCurrentWeather = async (city) => {
+    const baseUrl = "http://api.weatherbit.io/v2.0/current?";
+    const apiKey = "64e2aeb35084463da3c0fc13d427f7d5";
+    const url = `${baseUrl}key=${apiKey}&include=minutely&city=${city}`;
 
+    return await fetch(url)
+        .then((response) => response.json())
+        .then((json) => createCurrentTemp(json));
+};
 
-getGeonames('berlin').then( (json) => {
-    console.log(json);
-})
+const generateButtonClick = () => {
+    getCurrentWeather(inputCity.value).then((json) => updateUi(json));
+};
+
+generate.addEventListener("click", generateButtonClick);
+
+const createCurrentTemp = (dataJs) => {
+    return {
+        currentTemp: dataJs.data[0].temp,
+    };
+};
+
+const updateUi = (weather) => {
+    inputCity.value = "";
+
+    const currentTemp = document.getElementById("currentTemp");
+    // const min_temp = document.getElementById('min_temp')
+
+    currentTemp.innerHTML = `Today ${Math.ceil(weather.currentTemp)}°C`;
+};
+
+// const result = (dataJson) =>{
+
+//     currentTemp : dataJson.data[0].temp;
